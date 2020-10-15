55b88dd503b9754084c73b9bc758608898ae566d
Nate Komodo <31631620+NateKomodo@users.noreply.github.com>
2020-10-15 11:27:43 +0100

    Create ywallpaper.ps1

---
+++ ywallpaper.ps1
@@ -0,0 +1,27 @@
+(new-object System.Net.WebClient).DownloadFile('https://raw.githubusercontent.com/NateKomodo/DuckyScripts/main/Eq4TBON.jpg',"$Env:Temp\\b.jpg");
+Function Set-WallPaper($Image) {
+  
+Add-Type -TypeDefinition @" 
+using System; 
+using System.Runtime.InteropServices;
+  
+public class Params
+{ 
+    [DllImport("User32.dll",CharSet=CharSet.Unicode)] 
+    public static extern int SystemParametersInfo (Int32 uAction, 
+                                                   Int32 uParam, 
+                                                   String lpvParam, 
+                                                   Int32 fuWinIni);
+}
+"@ 
+  
+    $SPI_SETDESKWALLPAPER = 0x0014
+    $UpdateIniFile = 0x01
+    $SendChangeEvent = 0x02
+  
+    $fWinIni = $UpdateIniFile -bor $SendChangeEvent
+  
+    $ret = [Params]::SystemParametersInfo($SPI_SETDESKWALLPAPER, 0, $Image, $fWinIni)
+ 
+}
+Set-WallPaper("$Env:Temp\\b.jpg")
